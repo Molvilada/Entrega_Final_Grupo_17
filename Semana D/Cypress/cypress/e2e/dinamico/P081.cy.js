327f9f7461e11135527aaabbbe7fbc793f7d9fb5
Laura Molina <ld.molina11@uniandes.edu.co>
2023-05-25 19:58:12 -0500

    P081

---
+++ Semana D/Cypress/cypress/e2e/dinamico/P081.cy.js
@@ -0,0 +1,71 @@
+import PostSection from "../../support/elements/postsSection";
+import AdminMenu from "../../support/elements/adminMenu";
+import Site from "../../support/elements/site";
+import { mockarooService } from "../../support/services";
+
+const postSection = new PostSection();
+const adminMenu = new AdminMenu();
+const site = new Site();
+
+describe("Edición del campo Excerpt con caracteres especiales", () => {
+  it("Edición del campo Excerpt con caracteres especiales", () => {
+    /* 
+    -------------
+      GIVEN
+    -------------
+    */
+
+    // Autentica un usuario que puede crear posts
+    cy.login();
+
+    // Va a la pestaña Posts
+    adminMenu.postTab.click();
+    cy.wait(1000);
+
+    mockarooService("p081").then((res) => {
+      // Crea el post
+      const title = res.body.title;
+      const content = res.body.body;
+      postSection.createPost(title, content);
+
+      // Publica el post
+      postSection.publishPost();
+
+      /* 
+      -------------
+        WHEN
+      -------------
+      */
+
+      // Salir y volver a entrar al post
+      postSection.goBackToPostsSection.click();
+      postSection.postInList(title).click();
+
+      // Edita el exerpt
+      postSection.editorSettingsButton.click();
+      postSection.settingsExerpt.click();
+      const exerpt = res.body.excerpt;
+
+      postSection.settingsExerpt
+        .clear()
+        .type(exerpt, { parseSpecialCharSequences: false });
+      postSection.contentCover.click();
+      postSection.editorUpdateDropdown.click();
+      postSection.editorUpdateButton.click();
+      cy.wait(3000);
+
+      /* 
+      -------------
+        THEN
+      -------------
+      */
+      // Verifica que el exerpt aparezca visible en el post en el sitio
+      postSection.editorSettingsButton.click();
+      postSection.editorViewPost.invoke("attr", "href").then((href) => {
+        cy.visit(href);
+      });
+      cy.wait(1000);
+      site.postExerpt.contains(exerpt);
+    });
+  });
+});
